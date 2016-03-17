424400399b8b2808f29a09ed338af7eb8cd6b48b
giridhar <giridhar.reddy1992@gmail.com>
2016-03-17 18:08:32 +0530

    Initial Commit

---
+++ app/models/book.rb
@@ -0,0 +1,11 @@
+class Book < ActiveRecord::Base
+
+	def self.search(search)
+		if search
+			where(["name LIKE ?","%#{search}%"])
+		else
+			all
+		end
+	end
+
+end
---
+++ app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :name, :catagory, :price, :isn, :created_at, :updated_at
